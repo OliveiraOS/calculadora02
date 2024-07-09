2bd84d4dfadb7879e8ba1699c2fcd56c029f6004
OliveiraOS <105891073+OliveiraOS@users.noreply.github.com>
2024-07-09 18:09:12 -0300

    calculadora
    calculadora em C#

---
+++ calculadora/calculadora/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace calculadora
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num01, num02, menu, soma;
+
+            Console.WriteLine("Calculadora");
+            Console.WriteLine("Qual conta vc quer fazer:");
+            Console.WriteLine("1- Soma");
+            Console.WriteLine("2- Subtração");
+            Console.WriteLine("3- Multiplicação");
+            Console.WriteLine("4- Divisão");
+            menu =Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+
+            Console.WriteLine("Primeiro numero:");
+            num01 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Segundo numero:");
+            num02 = Convert.ToInt32(Console.ReadLine());
+
+            switch (menu)
+            {
+                case 1:
+                    soma = num01 + num02;
+
+                    Console.WriteLine("Resultado:" + soma);
+                    Thread.Sleep(2000);
+                    break;
+
+                case 2:
+                    soma = num01 - num02;
+
+                    Console.WriteLine("Resultado:" + soma);
+                    Thread.Sleep(2000);
+                    break;
+
+                case 3:
+                    soma = num01 * num02;
+
+                    Console.WriteLine("Resultado:" + soma);
+                    Thread.Sleep(2000);
+                    break;
+
+                case 4:
+                    soma = num01 / num02;
+
+                    Console.WriteLine("Resultado:" + soma);
+                    Thread.Sleep(2000);
+                    break;
+            }
+        }
+    }
+}
